ed3ae9cff7980beae2afcc111915a2709414d6af
nagalojan <145926100+nagalojan@users.noreply.github.com>
2023-11-02 19:02:00 +0530

    Update APIs.py
    api

--- APIs.py
+++ APIs.py
@@ -1,2 +1,3 @@
-## TugaRecon - APIs login and password...
-# Coded By skynet0x01
+# TugaRecon - certspotter, write by IT20077792
+# Bug Bounty Recon, search for subdomains and save in to a file
+# Coded for sliit-isp project
